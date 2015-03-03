2252b418523eb3dd8f98dc00b1c2d139cd3d93b5
Zousan <rompei0924@gmail.com>
2015-03-03 11:04:54 +0900

    Finished 5

--- cms/urls.py
+++ cms/urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns('',
     url(r'^book/add/$', views.book_edit, name='book_add'),  #登録
     url(r'^book/mod/(?P<book_id>\d+)/$', views.book_edit, name='book_mod'),  #修正
     url(r'^book/del/(?P<book_id>\d+)/$', views.book_del, name='book_del'),  #削除
-    )
+    url(r'^impression/(?P<book_id>\d+)/$', views.ImpressionList.as_view(), name='impression_list'),
+    url(r'^impression/add/(?P<book_id>\d+)/$', views.impression_edit, name='impression_add'),
+    url(r'^impression/mod/(?P<book_id>\d+)/(?P<impression_id>\d+)/$', views.impression_edit, name='impression_edit'),
+    url(r'^impression/del/(?P<book_id>\d+)/(?P<impression_id>\d+)/$', views.impression_del, name='impression_del'),)
--- cms/views.py
+++ cms/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, render_to_response, get_object_or_404,\
     redirect
 from django.http import HttpResponse
 from django.template.context import RequestContext
-from cms.forms import BookForm
-from cms.models import Book
+from cms.forms import BookForm, ImpressionForm
+from cms.models import Book, Impression
+from django.views.generic.list import ListView
+from django.db.transaction import commit
 
 # Create your views here.
 def book_list(request):
@@ -41,3 +43,45 @@ def book_del(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     book.delete()
     return redirect('cms:book_list')
+
+class ImpressionList(ListView):
+    '''感想の一覧'''
+    content_object_name='impressions'
+    template_name='cms/impression_list.html'
+    paginate_by=2
+    
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(Book, pk=kwargs['book_id'])
+        impressions = book.impressions.all().order_by('id')
+        self.object_list=impressions
+        
+        context = self.get_context_data(object_list=self.object_list, book=book)
+        return self.render_to_response(context)
+    
+def impression_edit(request, book_id, impression_id=None):
+    '''感想の編集'''
+    book = get_object_or_404(Book, pk=book_id)
+    if impression_id:
+        impression = get_object_or_404(Impression, pk=impression_id)
+    else:
+        impression = Impression()
+    
+    if request.method=='POST':
+        form = ImpressionForm(request.POST, instance=impression)
+        if form.is_valid():
+            impression = form.save(commit=False)
+            impression.book = book
+            impression.save()
+            return redirect('cms:impression_list', book_id=book_id)
+    else:
+        form = ImpressionForm(instance=impression)
+        
+    return render_to_response('cms/impression_edit.html',
+                              dict(form=form, book_id=book_id, impression_id= impression_id),
+                              context_instance=RequestContext(request))
+
+def impression_del(request, book_id, impression_id):
+    '''感想の削除'''
+    impression = get_object_or_404(Impression, pk=impression_id)
+    impression.delete()
+    return redirect('cms:impression_list', book_id=book_id)
